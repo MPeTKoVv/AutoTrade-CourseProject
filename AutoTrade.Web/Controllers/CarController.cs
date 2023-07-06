6eeeb11ce4aa3b1ffd04d325d426b0a3f5745aaa
MPeTKoVv <petkov.martin19@gmail.com>
2023-07-06 13:53:20 +0300

    Add CarController

---
+++ AutoTrade.Web/Controllers/CarController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AutoTrade.Web.Controllers
+{
+	public class CarController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
