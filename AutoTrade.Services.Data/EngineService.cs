3098d6a7eb8b244409e10501070fb6d47245a1d0
MPeTKoVv <petkov.martin19@gmail.com>
2023-07-29 02:24:20 +0300

    Added EngineService

---
+++ AutoTrade.Services.Data/EngineService.cs
@@ -0,0 +1,31 @@
+﻿using AutoTrade.Services.Data.Interfaces;
+using AutoTrade.Web.Data;
+using AutoTrade.Web.ViewModels.Engine;
+using Microsoft.EntityFrameworkCore;
+
+namespace AutoTrade.Services.Data
+{
+	public class EngineService : IEngineService
+	{
+		private readonly AutoTradeDbContext dbContext;
+
+        public EngineService(AutoTradeDbContext dbContext)
+        {
+			this.dbContext = dbContext;
+        }
+
+        public async Task<IEnumerable<CarSelectEngineTypeViewModel>> AllEngineTypesAsync()
+		{
+			IEnumerable<CarSelectEngineTypeViewModel> allEngineTypes = await this.dbContext
+				.EngineTypes
+				.Select(e => new CarSelectEngineTypeViewModel
+				{
+					Id = e.Id,
+					Type = e.Type
+				})
+				.ToArrayAsync();
+
+			return allEngineTypes;
+		}
+	}
+}
---
+++ AutoTrade.Services.Data/Interfaces/IEngineService.cs
@@ -0,0 +1,14 @@
+﻿using AutoTrade.Web.ViewModels.Engine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoTrade.Services.Data.Interfaces
+{
+	public interface IEngineService
+	{
+		Task<IEnumerable<CarSelectEngineTypeViewModel>> AllEngineTypesAsync();
+	}
+}
