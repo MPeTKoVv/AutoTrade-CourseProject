fcfa7074afc6698acf377064b3624e24d1a0e42c
MPeTKoVv <petkov.martin19@gmail.com>
2023-08-08 16:44:31 +0300

    Add summary to the extension method for seeding administrator

--- AutoTrade.Web/Program.cs
+++ AutoTrade.Web/Program.cs
@@ -1,83 +1,86 @@
 namespace AutoTrade.Web
 {
 	using Microsoft.AspNetCore.Mvc;
-    using Microsoft.EntityFrameworkCore;
+	using Microsoft.EntityFrameworkCore;
 	using Microsoft.AspNetCore.Identity;
-	
-    using Data;
-    using AutoTrade.Data.Models;
-    using Services.Data.Interfaces;
-    using Web.Infrstructure.Extensions;
+
+	using Data;
+	using AutoTrade.Data.Models;
+	using Services.Data.Interfaces;
+	using Web.Infrstructure.Extensions;
 	using Web.Infrastructure.ModelBinders;
 
-    using static Common.GeneralApplicationConstants;
+	using static Common.GeneralApplicationConstants;
 
 	public class Program
-    {
-        public static void Main(string[] args)
-        {
-            WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+	{
+		public static void Main(string[] args)
+		{
+			WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
-            string connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+			string connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
 
-            builder.Services.AddDbContext<AutoTradeDbContext>(options =>
-                options.UseSqlServer(connectionString));
+			builder.Services.AddDbContext<AutoTradeDbContext>(options =>
+				options.UseSqlServer(connectionString));
 
-            builder.Services.AddDefaultIdentity<ApplicationUser>(options =>
-            {
-                options.SignIn.RequireConfirmedAccount = builder.Configuration.GetValue<bool>("Identity:SignIn:RequireConfirmedAccount");
-                options.Password.RequireLowercase = builder.Configuration.GetValue<bool>("Identity:Password:RequireLowercase");
-                options.Password.RequireUppercase = builder.Configuration.GetValue<bool>("Identity:Password:RequireUppercase");
-                options.Password.RequireNonAlphanumeric = builder.Configuration.GetValue<bool>("Identity:Password:RequireNonAlphanumeric");
-                options.Password.RequiredLength = builder.Configuration.GetValue<int>("Identity:Password:RequiredLength");
-            })
-                .AddRoles<IdentityRole<Guid>>()
-                .AddEntityFrameworkStores<AutoTradeDbContext>();
+			builder.Services.AddDefaultIdentity<ApplicationUser>(options =>
+			{
+				options.SignIn.RequireConfirmedAccount = builder.Configuration.GetValue<bool>("Identity:SignIn:RequireConfirmedAccount");
+				options.Password.RequireLowercase = builder.Configuration.GetValue<bool>("Identity:Password:RequireLowercase");
+				options.Password.RequireUppercase = builder.Configuration.GetValue<bool>("Identity:Password:RequireUppercase");
+				options.Password.RequireNonAlphanumeric = builder.Configuration.GetValue<bool>("Identity:Password:RequireNonAlphanumeric");
+				options.Password.RequiredLength = builder.Configuration.GetValue<int>("Identity:Password:RequiredLength");
+			})
+				.AddRoles<IdentityRole<Guid>>()
+				.AddEntityFrameworkStores<AutoTradeDbContext>();
 
-            builder.Services.AddApplicationServices(typeof(ICarService));
+			builder.Services.AddApplicationServices(typeof(ICarService));
 
-            builder.Services.ConfigureApplicationCookie(cfg =>
-            {
-                cfg.LoginPath = "/User/Login";
-            });
+			builder.Services.ConfigureApplicationCookie(cfg =>
+			{
+				cfg.LoginPath = "/User/Login";
+			});
 
-            builder.Services
-                .AddControllersWithViews()
-                .AddMvcOptions(options =>
-                {
-                    options.ModelBinderProviders.Insert(0, new DecimalModelBinderProvider());
-                    options.Filters.Add<AutoValidateAntiforgeryTokenAttribute>();
-                });
+			builder.Services
+				.AddControllersWithViews()
+				.AddMvcOptions(options =>
+				{
+					options.ModelBinderProviders.Insert(0, new DecimalModelBinderProvider());
+					options.Filters.Add<AutoValidateAntiforgeryTokenAttribute>();
+				});
 
-            WebApplication app = builder.Build();
+			WebApplication app = builder.Build();
 
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseMigrationsEndPoint();
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Home/Error/500");
-                app.UseStatusCodePagesWithRedirects("/Home/Error?statusCode={0}");
+			if (app.Environment.IsDevelopment())
+			{
+				app.UseMigrationsEndPoint();
+				app.UseDeveloperExceptionPage();
+			}
+			else
+			{
+				app.UseExceptionHandler("/Home/Error/500");
+				app.UseStatusCodePagesWithRedirects("/Home/Error?statusCode={0}");
 
-                app.UseHsts();
-            }
+				app.UseHsts();
+			}
 
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
+			app.UseHttpsRedirection();
+			app.UseStaticFiles();
 
-            app.UseRouting();
+			app.UseRouting();
 
-            app.UseAuthentication();
-            app.UseAuthorization();
+			app.UseAuthentication();
+			app.UseAuthorization();
 
-            app.SeedAdministrator(DevelopmentAdminEmail);
+			if (app.Environment.IsDevelopment())
+			{
+				app.SeedAdministrator(DevelopmentAdminEmail);
+			}
 
-            app.MapDefaultControllerRoute();
-            app.MapRazorPages();
+			app.MapDefaultControllerRoute();
+			app.MapRazorPages();
 
-            app.Run();
-        }
-    }
+			app.Run();
+		}
+	}
 }
