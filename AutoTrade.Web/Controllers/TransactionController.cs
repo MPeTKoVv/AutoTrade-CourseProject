f5ced614ee908579feca0ccf6b884d9bd0d2714a
MPeTKoVv <petkov.martin19@gmail.com>
2023-08-11 03:52:19 +0300

    Add buttons for SoldCars and BoughtCars

---
+++ AutoTrade.Web/Controllers/TransactionController.cs
@@ -0,0 +1,47 @@
+﻿namespace AutoTrade.Web.Controllers
+{
+	using Microsoft.AspNetCore.Mvc;
+	using Microsoft.AspNetCore.Authorization;
+
+	using Services.Data.Interfaces;
+	using ViewModels.Car;
+	using Infrastructure.Extensions;
+
+	using static Common.NotificationMessagesConstants;
+
+	[Authorize]
+	public class TransactionController : Controller
+	{
+		private readonly ISellerService sellerService;
+		private readonly ICarService carService;
+
+		public TransactionController(ISellerService sellerService, ICarService carService)
+		{
+			this.sellerService = sellerService;
+			this.carService = carService;
+		}
+
+		public async Task<IActionResult> MySoldCars()
+		{
+			List<CarAllViewModel> myCars = new List<CarAllViewModel>();
+
+			return View(myCars);
+		}
+
+		public async Task<IActionResult> MyBoughtCars()
+		{
+			List<CarAllViewModel> myCars = new List<CarAllViewModel>();
+
+			return View(myCars);
+		}
+
+		private IActionResult GeneralError()
+		{
+			TempData[ErrorMessage] =
+				"Unexpected error occurred! Please try again later or contact administrator";
+
+			return RedirectToAction("Index", "Home");
+		}
+
+	}
+}
